463dd7c76137b33554b27a3a8cff10dbfdefe2a0
monogon <maxduhmine@gmail.com>
2023-05-28 16:12:16 +0300

    added frontend timestemp support

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,7 @@
 let input = document.getElementById("input");
 let output = document.getElementById("output");
 let number = 0;
+let last_timestamp = 0;
 
 document.getElementById("send").addEventListener("click", function () {
 
@@ -13,8 +14,18 @@ document.getElementById("send").addEventListener("click", function () {
 
    
 });
+get_timestemp(){
+    fetch("http://localhost:1290/timestamp", {method: "GET"}).then(async (response)=> {
+        await response.json().then(async (value) => {
+            console.log(value.value);
+            last_timestamp = value.value;
+        });
+    });
+}
 function update(){
+
     output.innerHTML = "Your number is "+number;
+    console.log("Last timestamp: "+last_timestamp);
     setTimeout(update, 1/10);
 }
 
