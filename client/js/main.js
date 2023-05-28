fb3b0fa065aac6861e641f99dfbb37ac13d12c7c
monogon <maxduhmine@gmail.com>
2023-05-28 17:45:21 +0300

    added parse_character_params

--- client/js/main.js
+++ client/js/main.js
@@ -1,24 +1,30 @@
 let input = document.getElementById("input");
 let output = document.getElementById("output");
 let number = 0;
-let id;
+let id=1;
 let last_timestamp = 0;
 
-function inject(){
-    fetch("http://localhost:1290/characters/inject?id="+id, {method: "GET"}).then(async (response)=> {
-        await response.json().then(async (response_data) => {
-            last_timestamp = response_data.timestamp;
-        });
-    }
-    );
+
+class Character{
+    id;
+    name;
 }
 
+let character = new Character();
+
+function parse_character_params(params){
+    console.log(params);
+    console.log("Not implemented");
+}
+
+
+
 function check_for_updates(){
-    fetch("http://localhost:1290/characters/check-update?id="+id+"&timestamp="+last_timestamp, {method: "GET"}).then(async (response)=> {
+    fetch("http://93.175.234.30:1290/characters/check-update?id="+id, {method: "GET"}).then(async (response)=> {
         await response.json().then(async (response_data) => {
-            console.log(value.timestamp);
             if (last_timestamp != response_data.timestamp){
                 last_timestamp = response_data.timestamp;
+                parse_character_params(response_data.params);
             }else{
                 console.log("No updates");
             }
@@ -26,7 +32,7 @@ function check_for_updates(){
     });
 }
 function send_update(param_name, value){
-    fetch("http://localhost:1290/characters/send-update?id="+id+"&param-name="+param_name+"&value="+value, {method: "GET"}).then(async (response)=> {
+    fetch("http://93.175.234.30:1290/characters/send-update?id="+id+"&param-name="+param_name+"&value="+value, {method: "GET"}).then(async (response)=> {
         await response.json().then(async (response_data) => {
             last_timestamp = response_data.timestamp; 
             console.log(last_timestamp);
@@ -39,6 +45,6 @@ function update(){
 
 
     console.log("Last timestamp: "+last_timestamp);
-    setTimeout(update, 1000/10);
+    setTimeout(update, 1000/1);
 }
 update();
