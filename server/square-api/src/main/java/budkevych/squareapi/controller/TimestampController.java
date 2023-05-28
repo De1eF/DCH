0e9c7d80eda43d0944c552b1d8e1481fe18c52d9
De1eF <jatfun58@gmail.com>
2023-05-28 15:25:09 +0200

    Added CrossOrigin annotation to TimestampController

--- server/square-api/src/main/java/budkevych/squareapi/controller/TimestampController.java
+++ server/square-api/src/main/java/budkevych/squareapi/controller/TimestampController.java
@@ -1,11 +1,13 @@
 package budkevych.squareapi.controller;
 
 import budkevych.squareapi.dto.TimestampResponseDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://127.0.0.1:5500")
 @RequestMapping("/timestamp")
 public class TimestampController {
     @GetMapping
