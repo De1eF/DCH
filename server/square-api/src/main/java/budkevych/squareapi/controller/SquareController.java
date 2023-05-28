f49eaae3299652547dcf585d2185e6a3c4d801e6
De1eF <jatfun58@gmail.com>
2023-05-28 14:17:05 +0200

    Added CrossOrigin annotation to fix CORS error

--- server/square-api/src/main/java/budkevych/squareapi/controller/SquareController.java
+++ server/square-api/src/main/java/budkevych/squareapi/controller/SquareController.java
@@ -1,11 +1,13 @@
 package budkevych.squareapi.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:5500")
 @RequestMapping("square")
 public class SquareController {
     @GetMapping("/{num}")
