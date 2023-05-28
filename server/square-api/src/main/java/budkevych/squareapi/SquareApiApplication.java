b847811f8828114c29ae596af89fecf43e55e3a1
De1eF <jatfun58@gmail.com>
2023-05-28 13:32:13 +0200

    commit

---
+++ server/square-api/src/main/java/budkevych/squareapi/SquareApiApplication.java
@@ -0,0 +1,13 @@
+package budkevych.squareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareApiApplication.class, args);
+	}
+
+}
---
+++ server/square-api/src/test/java/budkevych/squareapi/SquareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package budkevych.squareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
