bd1e19bb6adaf5bb5628355eca7319dc4f5cc997
monogon <maxduhmine@gmail.com>
2023-05-28 16:08:42 +0300

    finished

--- client/js/main.js
+++ client/js/main.js
@@ -1,17 +1,22 @@
-
 let input = document.getElementById("input");
 let output = document.getElementById("output");
 let number = 0;
 
 document.getElementById("send").addEventListener("click", function () {
 
-    fetch("http://localhost:1290/square/"+input.value, {method: "GET"}).then(function (response) {
-        console.log(response);
-        number = response;
-        return response;
+    fetch("http://localhost:1290/square/"+input.value, {method: "GET"}).then(async (response)=> {
+        await response.json().then(async (value) => {
+            console.log(value.value);
+            number = value.value;
+            
+        });
     });
 
-    output.innerHTML = "Your number is "+number;
-
-    
+   
 });
+function update(){
+    output.innerHTML = "Your number is "+number;
+    setTimeout(update, 1/10);
+}
+
+update();
