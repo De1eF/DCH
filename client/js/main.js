ffd46de7104dabad10e4b580ef9effcd3b5f52d0
monogon <maxduhmine@gmail.com>
2023-05-28 15:05:17 +0300

    .

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,16 @@
 
 let input = document.getElementById("input");
 let output = document.getElementById("output");
+let number = 0;
 
 document.getElementById("send").addEventListener("click", function () {
 
-    output.innerHTML = "Your number is "+input.value;
+    fetch("http://localhost:1290/square/"+input.value, {method: "GET"}).then(function (response) {
+        console.log(response);
+        number = response;
+        return response;
+    });
+
+    output.innerHTML = "Your number is "+number;
+
 });
