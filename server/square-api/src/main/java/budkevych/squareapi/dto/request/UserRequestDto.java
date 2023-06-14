5acb5608837d92dd6eab14c4938153eefc6044e7
De1eF <jatfun58@gmail.com>
2023-06-14 15:50:21 +0200

    UserRequestDto username not blank

--- server/square-api/src/main/java/budkevych/squareapi/dto/request/UserRequestDto.java
+++ server/square-api/src/main/java/budkevych/squareapi/dto/request/UserRequestDto.java
@@ -2,8 +2,6 @@ package budkevych.squareapi.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -13,5 +11,6 @@ public class UserRequestDto {
     private String email;
     @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
