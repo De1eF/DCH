2400793a4bf9aabae0090bbfc959f0e1cd622d9b
De1eF <jatfun58@gmail.com>
2023-06-28 22:01:16 +0200

    Changed password constraints

--- server/dcs-api/src/main/java/budkevych/dcsapi/dto/request/UserLoginRequestDto.java
+++ server/dcs-api/src/main/java/budkevych/dcsapi/dto/request/UserLoginRequestDto.java
@@ -1,12 +1,13 @@
 package budkevych.dcsapi.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserLoginRequestDto {
     @NotBlank(message = "Login can't be null or blank!")
     private String login;
-    @NotBlank(message = "Password can't be null or blank!")
+    @Size(min = 5, message = "Password must be at least 5 symbols")
     private String password;
 }
