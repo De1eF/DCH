ee325a991144f0a8b80317ce0266374ff8c9dfbb
De1eF <jatfun58@gmail.com>
2023-07-13 21:29:24 +0200

    Project structure

--- new-client/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Testing of pages</h1>
-    <img alt="Hotdog" src="../assets/hotdog.jpg">
-  </div>
-</template>
--- new-client/src/views/NotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="about">
-        <h1>404</h1>
-        <h2>Page not Found</h2>
-        <img alt="Hotdog" src="../assets/NotFound.webp">
-    </div>
-</template>
